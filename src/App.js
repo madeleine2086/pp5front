28ab5c5f05ab5a8d21710b666650b82d48de4ec2
Magdalena Dmitruk <dmitrukm@gmail.com>
2024-07-19 12:06:29 +0000

    running npm install to update lock file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Magda, all is fine! You can an will do it!!</h1>
+      <h1>Hello Magda, all is fine! You can and will do it!!</h1>
     </div>
   );
 }
