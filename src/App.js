4febfea900412057f7d55f4f56d8219125c72b0e
Magdalena Dmitruk <dmitrukm@gmail.com>
2024-07-19 12:20:19 +0000

    testing if react bootstrap is working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Magda, all is fine! You can and will do it!!</h1>
+      <Button variant="secondary">Button</Button>
     </div>
   );
 }
