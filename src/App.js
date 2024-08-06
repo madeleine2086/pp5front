53dae11967a15d817a677d5178989f6a62c73a3b
Magdalena Dmitruk <dmitrukm@gmail.com>
2024-08-06 16:13:10 +0000

    added header to profile page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import PostsPage from "./pages/posts/PostsPage";
 import PostEditForm from "./pages/posts/PostEditForm";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import NotFound from "./components/NotFound";
+import ProfilePage from "./pages/profiles/ProfilePage";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -52,6 +53,7 @@ function App() {
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
+          <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
           <Route render={() => <NotFound />} />
         </Switch>
       </Container>
